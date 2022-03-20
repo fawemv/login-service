9c20194c651d5c9388de56399eddbfe4fe1de0ae
陈浪 <3223818229@qq.com>
2022-03-20 22:04:50 +0800

    登录账号查询

---
+++ src/main/java/com/example/loginservice/mapper/SysUserMapper.java
@@ -0,0 +1,19 @@
+package com.example.loginservice.mapper;
+
+import com.example.loginservice.pojo.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 陈浪
+ * @since 2022-03-20
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+    //@Select("SELECT * FROM `sysuser` where username = #{username}")
+    SysUser getByUsername(String username);
+
+}
